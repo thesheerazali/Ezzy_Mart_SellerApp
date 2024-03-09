fa9751f249938e15c381627fb94656692bed2fea
thesheerazali <sheerazali7676@gmail.com>
2024-03-09 22:43:13 +0500

    popup added

--- lib/views/product_screen/product_screen.dart
+++ lib/views/product_screen/product_screen.dart
@@ -26,10 +26,37 @@ class ProductScreen extends StatelessWidget {
                 children: List.generate(
                     20,
                     (index) => ListTile(
-                        leading: Image.asset(imgProduct),
-                        title: boldText(
-                            text: "Product Title", color: fontGrey, size: 14.0),
-                        subtitle: normalText(text: "\$40", color: darkGrey)))),
+                          leading: Image.asset(imgProduct),
+                          title: boldText(
+                              text: "Product Title",
+                              color: fontGrey,
+                              size: 14.0),
+                          subtitle: normalText(text: "\$40", color: darkGrey),
+                          trailing: VxPopupMenu(
+                              arrowSize: 14,
+                              menuBuilder: () => Column(
+                                    children: List.generate(
+                                        popipManuIcons.length,
+                                        (index) => Padding(
+                                              padding:
+                                                  const EdgeInsets.all(12.0),
+                                              child: Row(
+                                                children: [
+                                                  Icon(
+                                                    popipManuIcons[index],
+                                                  ),
+                                                  10.widthBox,
+                                                  normalText(
+                                                      text:
+                                                          popipManuTitle[index],
+                                                      color: fontGrey)
+                                                ],
+                                              ).onTap(() {}),
+                                            )),
+                                  ).box.rounded.white.width(150).make(),
+                              clickType: VxClickType.singleClick,
+                              child: const Icon(Icons.more_vert_rounded)),
+                        ))),
           )),
     );
   }
