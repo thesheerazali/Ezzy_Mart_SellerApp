a54c4b02361d82ce2ccd5f0a6e005c805d12124c
thesheerazali <sheerazali7676@gmail.com>
2024-03-10 14:54:39 +0500

    profile ui complete

--- lib/views/widgets/custom_textfield.dart
+++ lib/views/widgets/custom_textfield.dart
@@ -2,8 +2,9 @@ import 'package:ezzy_mart_seller_app/views/widgets/text_widget.dart';
 
 import '../../const/const.dart';
 
-Widget customTextField({hint, label, controller}) {
+Widget customTextField({hint, label, controller, isDesc = false}) {
   return TextFormField(
+      maxLines: isDesc ? 4 : 1,
       decoration: InputDecoration(
           isDense: true,
           label: normalText(text: label),
